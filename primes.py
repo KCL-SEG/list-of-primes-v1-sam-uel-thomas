9eb0c36bd8d58800672d3b77d2fa2d27c9f55b6f
Sam Thomas <samuel.mmthomas@gmail.com>
2023-10-04 14:36:16 +0100

    Add ability to the list of primes of chose length

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+    index = 2
+    while number_of_primes >= 0:
+        if checkPrime(index):
+            list.append(index)
+            number_of_primes -= 1
+        index += 1
     return list
+
+def checkPrime(number):
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
